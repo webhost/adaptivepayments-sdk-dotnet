a3c3e8e834e8c491da8373da5aa271e760c07da6
unknown <rsomaskandan@D-MAA-00391891.corp.ebay.com>
2012-12-26 18:07:14 +0530

    Removing underscore from .NET SDKs as part of .NET Standards

--- AdaptivePayments.bat
+++ AdaptivePayments.bat
@@ -1,3 +1,3 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio 8\Common7\IDE\devenv.com" PayPal_AdaptivePayments_SDK\PayPal_AdaptivePayments_SDK.sln /build Release %1
 
-copy /Y PayPal_AdaptivePayments_SDK\bin\Release\PayPal_AdaptivePayments_SDK.dll Samples\AdaptivePaymentsSampleApp\lib\. 
+copy /Y PayPal_AdaptivePayments_SDK\bin\Release\PayPalAdaptivePaymentsSDK.dll Samples\AdaptivePaymentsSampleApp\lib\. 
